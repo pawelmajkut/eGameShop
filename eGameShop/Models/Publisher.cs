147d9ee086ca8be38b6d49ae6aa0738ecbe04457
pawelmajkut <Pawel.Majkut@microsoft.wsei.edu.pl>
2023-02-11 19:11:16 +0100

    Update Publisher.cs

--- eGameShop/Models/Publisher.cs
+++ eGameShop/Models/Publisher.cs
@@ -1,8 +1,14 @@
-﻿namespace eGameShop.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace eGameShop.Models
 {
     public class Publisher
     {
+        [Key]
+        public int Id { get; set; }
+
         public string ProfilePictureURL { get; set; }
         public string FullName { get; set; }
+        public string Description { get; set; }
     }
 }
