93cd178f68a0b313db50f55092463c44eef7a3a0
pawelmajkut <Pawel.Majkut@microsoft.wsei.edu.pl>
2023-02-13 23:03:45 +0100

    changes

--- eGameShop/Controllers/HomeController.cs
+++ eGameShop/Controllers/HomeController.cs
@@ -18,15 +18,15 @@ namespace eGameShop.Controllers
             return View();
         }
 
-        public IActionResult Privacy()
-        {
-            return View();
-        }
+        //public IActionResult Privacy()
+        //{
+        //    return View();
+        //}
 
-        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
-        public IActionResult Error()
-        {
-            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
-        }
+        //[ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        //public IActionResult Error()
+        //{
+        //    return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        //}
     }
 }
--- eGameShop/Program.cs
+++ eGameShop/Program.cs
@@ -78,7 +78,7 @@ app.UseEndpoints(endpoints =>
 {
     endpoints.MapControllerRoute(
         name: "default",
-        pattern: "{controller=Home}/{action=Index}/{id?}");
+        pattern: "{controller=Games}/{action=Index}/{id?}");
 });
 
 //Seed database
